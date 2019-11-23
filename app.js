9d57226d3c81f8ed71a0c5aa3ff82ae8d43e9c1d
llabel01 <54954000+llabel01@users.noreply.github.com>
2019-11-23 15:45:29 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,215 @@
+const Joi = require('joi'); 
+const express = require('express');
+const app = express()
+const mysql = require('mysql');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// edit a review
+
+// Create connection
+const db = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'IhC2sM!!ah',
+  database : 'recipes'
+});
+
+/* Get all users */
+app.get('/api/users', (req, res) => {
+    const sql = 'select * from user';
+    const query = db.query(sql, (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get a particular user */
+app.get('/api/users/:username', (req, res) => {
+    const sql = 'select * from user where username = ?';
+    const query = db.query(sql, [req.params.username], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get all recipes */
+app.get('/api/recipes', (req, res) => {
+    const sql = 'select * from recipe';
+    const query = db.query(sql, (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get a particular recipe */
+app.get('/api/recipes/:recipe_id', (req, res) => {
+    const sql = 'select * from recipe where recipe_id = ?';
+    const query = db.query(sql, [req.params.recipe_id], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get all reviews */
+app.get('/api/reviews', (req, res) => {
+    const sql = 'select * from review';
+    const query = db.query(sql, (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get a particular review */
+app.get('/api/reviews/:username/:recipe_id', (req, res) => {
+    const sql = 'select * from review where username = ? and recipe_id = ?';
+    const query = db.query(sql, [req.params.username, req.params.recipe_id], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get all reviews for a particular recipe */
+app.get('/api/reviews/:recipe_id', (req, res) => {
+    const sql = 'select * from review where recipe_id = ?';
+    const query = db.query(sql, [req.params.recipe_id], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get all saves */
+app.get('/api/saves', (req, res) => {
+    const sql = 'select * from saved';
+    const query = db.query(sql, (err, results) => {
+        if (err) return res.status(400).send(err.code);
+        res.send(results); 
+    });
+});
+
+/* Get a particular save */
+app.get('/api/saves/:username/:recipe_id', (req, res) => {
+    const sql = 'select * from saved where username = ? and recipe_id = ?';
+    const query = db.query(sql, [req.params.username, req.params.recipe_id], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Get all recipes a user has saved */
+app.get('/api/saves/:username/', (req, res) => {
+    const sql = 'select * from saved where username = ?';
+    const query = db.query(sql, [req.params.username], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+/* Insert a user */
+app.post('/api/users', (req, res) => {
+    const { error } = validateUser(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = req.body;
+    const sql = 'insert into user set ?';
+    const query = db.query(sql, user, (err, result) => {
+        if (err) return res.status(400).send(err.code);
+        res.send(user); /* Maybe should return the result? */
+    });
+});
+
+/* Insert a recipe */
+app.post('/api/recipes', (req, res) => {
+    const { error } = validateRecipe(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const recipe = req.body;
+    const sql = 'insert into recipe set ?';
+    const query = db.query(sql, recipe, (err, result) => {
+        if (err) return res.status(400).send(err.code);
+        res.send(recipe);
+    });
+});
+
+/* Insert a review */
+app.post('/api/reviews', (req, res) => {
+    const { error } = validateReview(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const review = req.body;
+    const sql = 'insert into review set ?';
+    const query = db.query(sql, review, (err, result) => {
+        if (err) return res.status(400).send(err.code);
+        res.send(review);
+    });
+});
+
+/* Insert a "save" */
+app.post('/api/saves', (req, res) => {
+    const { error } = validateSave(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const save = req.body;
+    const sql = 'insert into saved set ?';
+    const query = db.query(sql, save, (err, result) => {
+        if (err) return res.status(400).send(err.code);
+        res.send(save);
+    });
+});
+
+/* Delete a save */
+app.delete('/api/saves/:username/:recipe_id', (req, res) => {
+    const sql = 'delete from saved where username = ? and recipe_id = ?';
+    const query = db.query(sql, [req.params.username, req.params.recipe_id], (err, results) => {
+        if (err) return res.status(400).send(err.code)
+        res.send(results); 
+    });
+});
+
+function validateUser(user) {
+    const schema = {
+        username: Joi.string().required().max(255),
+        firstname: Joi.string().required().max(255),
+        lastname: Joi.string().required().max(255),
+        password: Joi.string().required().max(255)
+    };
+
+    return Joi.validate(user, schema);
+};
+
+function validateRecipe(recipe) {
+    const schema = {
+        recipe_id: Joi.number().integer().required()
+    };
+
+    return Joi.validate(recipe, schema);
+};
+
+function validateReview(review) {
+    const schema = {
+        rating: Joi.number().integer().min(0).max(5).required(),
+        upvote: Joi.boolean().required(),
+        review: Joi.string().max(1500).required(),
+        username: Joi.string().max(255).required(),
+        recipe_id: Joi.number().integer().required()
+    };
+
+    return Joi.validate(review, schema);
+};
+
+function validateSave(save) {
+    const schema = {
+        username: Joi.string().max(255).required(),
+        recipe_id: Joi.number().integer().required(),
+    };
+
+    return Joi.validate(save, schema);
+};
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
