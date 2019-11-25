07a090403f8d6844c8447e20b254285f27b356da
Andrew Cervantes <45413115+AJCervantes@users.noreply.github.com>
2019-11-25 09:10:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -211,5 +211,6 @@ function validateSave(save) {
 };
 
 const port = process.env.PORT || 3000
+app.use(express.static("public"));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
