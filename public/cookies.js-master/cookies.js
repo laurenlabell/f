bb401aeeae7f5415fb4ece1e7f578727aad7bc56
llabel01 <54954000+llabel01@users.noreply.github.com>
2019-11-30 19:10:39 -0500

    Update cookies.js

--- public/cookies.js-master/cookies.js
+++ public/cookies.js-master/cookies.js
@@ -1,139 +1,136 @@
 /*\
 |*|
-|*|	:: cookies.js ::
+|*|  :: cookies.js ::
 |*|
-|*|	A complete cookies reader/writer framework with full unicode support.
+|*|  A complete cookies reader/writer framework with full unicode support.
 |*|
-|*|	Revision #7 - September 13th, 2019
+|*|  Revision #3 - July 13th, 2017
 |*|
-|*|	https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
-|*|	https://developer.mozilla.org/User:fusionchess
-|*|	https://github.com/madmurphy/cookies.js
+|*|  https://developer.mozilla.org/en-US/docs/Web/API/document.cookie
+|*|  https://developer.mozilla.org/User:fusionchess
+|*|  https://github.com/madmurphy/cookies.js
 |*|
-|*|	This framework is released under the GNU Public License, version 3 or later.
-|*|	http://www.gnu.org/licenses/gpl-3.0-standalone.html
+|*|  This framework is released under the GNU Public License, version 3 or later.
+|*|  http://www.gnu.org/licenses/gpl-3.0-standalone.html
 |*|
-|*|	Syntaxes:
+|*|  Syntaxes:
 |*|
-|*|	* docCookies.setItem(name, value[, end[, path[, domain[, secure[, same-site]]]]])
-|*|	* docCookies.getItem(name)
-|*|	* docCookies.removeItem(name[, path[, domain[, secure[, same-site]]]])
-|*|	* docCookies.hasItem(name)
-|*|	* docCookies.keys()
-|*|	* docCookies.clear([path[, domain[, secure[, same-site]]]])
+|*|  * docCookies.setItem(name, value[, end[, path[, domain[, secure]]]])
+|*|  * docCookies.getItem(name)
+|*|  * docCookies.removeItem(name[, path[, domain]])
+|*|  * docCookies.hasItem(name)
+|*|  * docCookies.keys()
 |*|
 \*/
 
-(function () {
+// updateUserDisplay()
+// updates the login/logout displays at top of page
 
-	function makeSetterString (sKey, sValue, vEnd, sPath, sDomain, bSecure, vSameSite) {
+// might be best to stringify the entire JSON recipe: 
+// setRecipe(recipe)
+// sets the recipe cookie to recipe
 
-		var sExpires = "";
+// getRecipe()
+// gets the recipe in the recipe cookie
 
-		if (vEnd) {
+// getSearch()
+// gets the value from the search cookie
 
-			switch (vEnd.constructor) {
+// setSearch(search)
+// sets the search cookie to search
 
-				case Number:
+// logout()
+// logs out the user (deletes the cookie)
 
-					/*
-					Note: Despite officially defined in RFC 6265, the use of `max-age` is not compatible with any
-					version of Internet Explorer, Edge and some mobile browsers. Therefore passing a number to
-					the end parameter might not work as expected. A possible solution might be to convert the the
-					relative time to an absolute time. For instance, replacing the following line with:
-					*/
-					/*
-					sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; expires=" + (new Date(vEnd * 1e3 + Date.now())).toUTCString();
-					*/
+// login(username)
+// sets the username cookie to username
 
-					sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
-					break;
+// signedIn()
+// returns true if someone is logged in and false otherwise
 
-				case String:
 
-					sExpires = "; expires=" + vEnd;
-					break;
-
-				case Date:
-
-					sExpires = "; expires=" + vEnd.toUTCString();
-					break;
-
-			}
-
-		}
-
-		return	encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "") + (!vSameSite || vSameSite.toString().toLowerCase() === "no_restriction" || vSameSite < 0 ? "" : vSameSite.toString().toLowerCase() === "lax" || Math.ceil(vSameSite) === 1 || vSameSite === true ? "; samesite=lax" : "; samesite=strict");
-
-	}
-
-	var reURIAllowed = /[\-\.\+\*]/g, reCNameAllowed = /^(?:expires|max\-age|path|domain|secure|samesite|httponly)$/i;
-
-	window.docCookies = {
-
-		"getItem": function (sKey) {
-
-			if (!sKey) { return null; }
-
-			return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(reURIAllowed, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
-
-		},
-
-		"setItem": function (sKey, sValue, vEnd, sPath, sDomain, bSecure, vSameSite) {
-
-			if (!sKey || reCNameAllowed.test(sKey)) { return false; }
-
-			document.cookie = makeSetterString(sKey, sValue, vEnd, sPath, sDomain, bSecure, vSameSite);
-			return true;
-
-		},
-
-		"removeItem": function (sKey, sPath, sDomain, bSecure, vSameSite) {
-
-			if (!this.hasItem(sKey)) { return false; }
-
-			document.cookie = makeSetterString(sKey, "", "Thu, 01 Jan 1970 00:00:00 GMT", sPath, sDomain, bSecure, vSameSite);
-			return true;
-
-		},
-
-		"hasItem": function (sKey) {
-
-			if (!sKey || reCNameAllowed.test(sKey)) { return false; }
-
-			return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(reURIAllowed, "\\$&") + "\\s*\\=")).test(document.cookie);
-
-		},
-
-		"keys": function () {
-
-			var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
-
-			for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
-
-				aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
-
-			}
-
-			return aKeys;
-		},
-
-		"clear": function (sPath, sDomain, bSecure, vSameSite) {
-
-			for (var aKeys = this.keys(), nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
-
-				this.removeItem(aKeys[nIdx], sPath, sDomain, bSecure, vSameSite);
-
-			}
-
-		}
-
-	};
-
-})();
-
-if (typeof module !== "undefined" && typeof module.exports !== "undefined") {
-
-	module.exports = docCookies;
-
-}
+var docCookies = {
+  updateUserDisplay: function () {
+    if (this.signedIn()) {
+        $("li.logout-btn").addClass("showBtn");
+        $("li.logout-btn").removeClass("hideBtn");
+        $("li.user-btns").addClass("hideBtn");
+        $("li.user-btns").removeClass("showBtn");
+    }
+    else {
+        $("li.logout-btn").addClass("hideBtn");
+        $("li.logout-btn").removeClass("showBtn");
+        $("li.user-btns").addClass("showBtn"); 
+        $("li.user-btns").removeClass("hideBtn");         
+    }
+  },
+  getRecipe: function () {
+    return this.getItem("recipe");
+  },
+  setRecipe: function (recipe) {
+    this.setItem("recipe", recipe);
+  },
+  getSearch: function () {
+    return this.getItem("search");
+  },
+  setSearch: function (search) {
+    this.setItem("search", search);
+  },
+  logout: function () { 
+    this.removeItem("username");
+    this.updateUserDisplay();
+  },
+  login: function (username) { 
+    this.setItem("username", username);
+    this.updateUserDisplay();
+  },
+  signedIn: function () { 
+    return this.hasItem("username");
+  },
+  getItem: function (sKey) {
+    if (!sKey) { return null; }
+    return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+  },
+  setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+    var sExpires = "";
+    if (vEnd) {
+      switch (vEnd.constructor) {
+        case Number:
+          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+          /*
+          Note: Despite officially defined in RFC 6265, the use of `max-age` is not compatible with any
+          version of Internet Explorer, Edge and some mobile browsers. Therefore passing a number to
+          the end parameter might not work as expected. A possible solution might be to convert the the
+          relative time to an absolute time. For instance, replacing the previous line with:
+          */
+          /*
+          sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; expires=" + (new Date(vEnd * 1e3 + Date.now())).toUTCString();
+          */
+          break;
+        case String:
+          sExpires = "; expires=" + vEnd;
+          break;
+        case Date:
+          sExpires = "; expires=" + vEnd.toUTCString();
+          break;
+      }
+    }
+    document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+    return true;
+  },
+  removeItem: function (sKey, sPath, sDomain) {
+    if (!this.hasItem(sKey)) { return false; }
+    document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+    return true;
+  },
+  hasItem: function (sKey) {
+    if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+    return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+  },
+  keys: function () {
+    var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+    for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+    return aKeys;
+  }
+};
