5c4e5ec076bc8109b219f095ee32351a7a9c567e
Olivia Ng <43549231+olivialng@users.noreply.github.com>
2019-12-01 20:44:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const mysql = require('mysql');
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
