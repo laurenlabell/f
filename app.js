ac296e8f64eb93a6158fa97b7023d46184073885
Andrew Cervantes <45413115+AJCervantes@users.noreply.github.com>
2019-12-02 20:39:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+//app.get('/', (req, res) => {
+//    res.send('Hello World');
+//});
 
 // edit a review
 
